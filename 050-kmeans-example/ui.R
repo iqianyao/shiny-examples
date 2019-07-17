a289d56ecbb70d80b91515ac249b639c0762ce97
liwenyu <lwyidname@163.com>
2019-07-17 17:53:26 +0800

    聚类分析页面更改为中文

--- 050-kmeans-example/ui.R
+++ 050-kmeans-example/ui.R
@@ -1,10 +1,10 @@
 pageWithSidebar(
-  headerPanel('Iris k-means clustering'),
+  headerPanel('聚类分析'),
   sidebarPanel(
-    selectInput('xcol', 'X Variable', names(iris)),
-    selectInput('ycol', 'Y Variable', names(iris),
+    selectInput('xcol', 'X轴', names(iris)),
+    selectInput('ycol', 'Y轴', names(iris),
                 selected=names(iris)[[2]]),
-    numericInput('clusters', 'Cluster count', 3,
+    numericInput('clusters', '节点数量', 3,
                  min = 1, max = 9)
   ),
   mainPanel(
